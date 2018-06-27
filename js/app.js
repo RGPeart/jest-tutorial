4e250344ecc39a1f769cf555b4fbc0671c96d12a
Joel Ross <joelross@uw.edu>
2018-06-27 13:04:46 -0700

    Add complete version

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /* A function that returns a greeting for the given name */
 function greet(name){
-  return "Hello,"+name;
+  return "Hello "+name[0].toUpperCase()+name.slice(1);
 }
 
 
@@ -11,7 +11,7 @@ function greet(name){
  * It may have a few small bugs...
  */
 function invertCase(text) {
-  const LOWER_CASE = "abedefghijklmnoqprstuvwxyz";
+  const LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
   const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   let tempString = '';
@@ -21,12 +21,15 @@ function invertCase(text) {
       let upper = UPPER_CASE[ LOWER_CASE.indexOf(char) ];
       tempString += upper;
     }
-    else { //is uppercase
-      let lower = LOWER_CASE [ LOWER_CASE.indexOf(char) ];
+    else if(UPPER_CASE.indexOf(char) != -1){ //is uppercase
+      let lower = LOWER_CASE [ UPPER_CASE.indexOf(char) ];
       tempString += lower;
     }
+    else {
+      tempString += char;
+    }
   }
-  return text;
+  return tempString;
 }
 
 
